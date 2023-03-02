ddbdc3fd7ffee6f8235fc2c243a48a1b0e305e43
mmayala <124645862+mmayala@users.noreply.github.com>
2023-03-01 22:41:14 -0500

    Update Readme.mm

--- Readme.mm
+++ Readme.mm
@@ -14,4 +14,4 @@ Este algoritmo es simple. Obtenemos la reversa del número a verificar (que
 solamente contiene dígitos [0-9]); a todos los números que ocupan una posición
 par se les debe multiplicar por dos, si este número es mayor o igual a 10,
 debemos sumar los dígitos del resultado; el número a verifica
-"C:\Users\danni\OneDrive\Pictures\ALGORITMO DE LUHN.png"
+![ALGORITMO DE LUHN](https://user-images.githubusercontent.com/124645862/222280324-dda6ec59-642e-44a1-9f4f-192f305d5a70.png)
