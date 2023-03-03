48232fe95de64bf0461c95f0f8c6776e9fea51cd
mmayalae <mmayalae@pichincha.com>
2023-03-02 22:18:28 -0500

    commit 7

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import validator from './validator.js'
 
+const imprimir = document.getElementById('imprimir')
+
 // cuando el usuario hace clic se mostrara el mensaje
 document.getElementById('validar').addEventListener('click', function(){
   const tarjeta = document.getElementById('tarjeta').value;
-  
+
  
-  //imprimir en la web si el resultado
-  const imprimir = document.getElementById('imprimir')
+  //imprimir en la web  el resultado
+  
   const resultadoTarjeta = validator.isValid(tarjeta);
   if (resultadoTarjeta === true){
     imprimir.innerHTML = 'tarjeta valida'
@@ -14,13 +16,14 @@ document.getElementById('validar').addEventListener('click', function(){
     imprimir.innerHTML = 'tarjeta invalida'
   }
 
+
   
   //enmascarar los 12 digitos de la tarjeta
   const cc = validator.maskify(tarjeta);
   document.getElementById('ocultar').innerHTML = cc;
 
-  
-});  
+});
+ 
 
   
   
